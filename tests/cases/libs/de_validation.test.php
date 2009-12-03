259fc1920c6b2a96cb76e1de171f50a069136149
ceeram <mayl@planet.nl>
2009-12-03 20:15:05 +0100

    added DeValidation rules and tests

---
+++ tests/cases/libs/de_validation.test.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * German Localized Validation class test case
+ * *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       localized
+ * @since         localized 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::import('Lib', 'Localized.DeValidation');
+
+class DeValidationTestCase extends CakeTestCase {
+/**
+ * test the postal method of DeValidation
+ *
+ * @return void
+ */
+	function testPostal() {
+		$this->assertTrue(DeValidation::postal('51109'));
+		$this->assertFalse(DeValidation::postal('051109'));
+	}
+}
