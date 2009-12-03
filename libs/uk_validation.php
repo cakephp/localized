f94b582f15a4df105b15d7459e43dc6675c8716e
ceeram <mayl@planet.nl>
2009-12-03 20:23:06 +0100

    added UkValidation rules and tests

---
+++ libs/uk_validation.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * UK Localized Validation class. Handles localized validation for The United Kingdom
+ * *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       localized
+ * @since         localized 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class UkValidation {
+/**
+ * Checks zipcodes for The United Kingdom
+ *
+ * @param string $check The value to check.
+ * @access public
+ * @return boolean
+ */
+	function postal($check) {
+		$pattern = '/\\A\\b[A-Z]{1,2}[0-9][A-Z0-9]? [0-9][ABD-HJLNP-UW-Z]{2}\\b\\z/i';
+		return preg_match($pattern, $check);
+	}
+}
---
+++ tests/cases/libs/uk_validation.test.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * UK Localized Validation class test case
+ * *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       localized
+ * @since         localized 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::import('Lib', 'Localized.UkValidation');
+
+class UkValidationTestCase extends CakeTestCase {
+/**
+ * test the postal method of UkValidation
+ *
+ * @return void
+ */
+	function testPostal() {
+		$this->assertTrue(UkValidation::postal('DT4 8PP'));
+		$this->assertFalse(UkValidation::postal('DT4-8PP'));
+	}
+}
