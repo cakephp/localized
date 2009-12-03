f337a6dd2057539c9b141d2c8dc2ebabbbb80d38
ceeram <mayl@planet.nl>
2009-12-03 20:18:58 +0100

    added EsValidation rules and tests

---
+++ libs/es_validation.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Spanish Localized Validation class. Handles localized validation for Spain
+ * *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       localized
+ * @since         localized 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class EsValidation {
+/**
+ * Checks phone numbers for Spain
+ *
+ * @param string $check The value to check.
+ * @access public
+ * @return boolean
+ */
+	function phone($check) {
+		$pattern = '/^\\+?(34[-. ]?)?\\(?(([689]{1})(([0-9]{2})\\)?[-. ]?|([0-9]{1})\\)?[-. ]?([0-9]{1}))|70\\)?[-. ]?([0-9]{1}))([0-9]{2})[-. ]?([0-9]{1})[-. ]?([0-9]{1})[-. ]?([0-9]{2})$/';
+		return preg_match($pattern, $check);
+	}
+}
---
+++ tests/cases/libs/es_validation.test.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Spanish Localized Validation class test case
+ * *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       localized
+ * @since         localized 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::import('Lib', 'Localized.EsValidation');
+
+class EsValidationTestCase extends CakeTestCase {
+/**
+ * test the phone method of EsValidation
+ *
+ * @return void
+ */
+	function testPhone() {
+		$this->assertTrue(EsValidation::phone('924227227'));
+		$this->assertFalse(EsValidation::phone('813 4567'));
+	}
+}
