a72f43264d585751d133884e144676d97884c229
Juan Basso <juan.basso@digitro.com.br>
2009-12-04 08:35:40 -0200

    added BrValidation rules and tests

---
+++ libs/br_validation.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Brazillian Localized Validation class. Handles localized validation for Brazil
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       localized
+ * @since         localized 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class BrValidation {
+	/**
+	 * Checks phone numbers for Brazil
+	 *
+	 * @param string $check The value to check.
+	 * @access public
+	 * @return boolean
+	 */
+	function phone($check) {
+		return (bool)preg_match('/^(\+?\d{1,3}? ?)?(\(0?\d{2}\) ?)?\d{4}[-. ]?\d{4}$/', $check);
+	}
+
+	/**
+	 * Checks zipcodes (CEP) for Brazil
+	 *
+	 * @param string $check The value to check.
+	 * @access public
+	 * @return boolean
+	 */
+	function postal($check) {
+		return (bool)preg_match('/^[0-9]{5}-?[0-9]{3}$/', $check);
+	}
+
+	/**
+	 * Checks cadastro de pessoa física (CPF) for Brazil
+	 *
+	 * @param string $check The value to check.
+	 * @access public
+	 * @return boolean
+	 */
+	function ssn($check) {
+		$check = str_replace(array(' ', '-', '.'), '', $check);
+		if (strlen($check) !== 11 || !ctype_digit($check)) {
+			return false;
+		}
+		$dv = substr($check, -2);
+		for ($pos = 9; $pos <= 10; $pos++) {
+			$sum = 0;
+			$position = $pos + 1;
+			for ($i = 0; $i <= $pos - 1; $i++, $position--) {
+				$sum += $check{$i} * $position;
+			}
+			$div = $sum % 11;
+			if ($div < 2) {
+				$check{$pos} = 0;
+			} else {
+				$check{$pos} = 11 - $div;
+			}
+		}
+		$dvRight = $check{9} * 10 + $check{10};
+		return $dvRight == $dv;
+	}
+
+}
+
+?>
---
+++ tests/cases/libs/br_validation.test.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Brazillian Localized Validation class test case
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       localized
+ * @since         localized 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::import('Lib', 'Localized.BrValidation');
+
+class BrValidationTest extends CakeTestCase {
+
+	/**
+	 * test the phone method of BrValidation
+	 *
+	 * @return void
+	 */
+	function testPhone() {
+		$this->assertFalse(BrValidation::phone('teststring'));
+		$this->assertFalse(BrValidation::phone('1-(33)-(333)-(4444)'));
+		$this->assertFalse(BrValidation::phone('1-(33)-3333-4444'));
+		$this->assertFalse(BrValidation::phone('1-(33)-33-4444'));
+		$this->assertFalse(BrValidation::phone('1-(33)-3-44444'));
+		$this->assertFalse(BrValidation::phone('1-(33)-3-444'));
+		$this->assertFalse(BrValidation::phone('1-(33)-3-44'));
+		$this->assertFalse(BrValidation::phone('2345678'));
+	
+		$this->assertTrue(BrValidation::phone('55 (48) 2345 6789'));
+		$this->assertTrue(BrValidation::phone('+55 (48) 2345 6789'));
+		$this->assertTrue(BrValidation::phone('+55 (048) 2345 6789'));
+		$this->assertTrue(BrValidation::phone('+55 (48) 2345-6789'));
+		$this->assertTrue(BrValidation::phone('+55 (48) 2345.6789'));
+		$this->assertTrue(BrValidation::phone('(48) 2345 6789'));
+		$this->assertTrue(BrValidation::phone('2345-6789'));
+		$this->assertTrue(BrValidation::phone('2345.6789'));
+		$this->assertTrue(BrValidation::phone('23456789'));
+	}
+
+	/**
+	 * test the postal method of BrValidation
+	 *
+	 * @return void
+	 */
+	function testPostal() {
+		$this->assertFalse(BrValidation::postal('111'));
+		$this->assertFalse(BrValidation::postal('1111'));
+		$this->assertFalse(BrValidation::postal('1234-123'));
+		$this->assertFalse(BrValidation::postal('12345-12'));
+
+		$this->assertTrue(BrValidation::postal('88000-123'));
+		$this->assertTrue(BrValidation::postal('01234-123'));
+		$this->assertTrue(BrValidation::postal('01234123'));
+	}
+
+	/**
+	 * test the ssn method of BrValidation
+	 *
+	 * @return void
+	 */
+	function testSsn() {
+		$this->assertFalse(BrValidation::ssn('22692173811'));
+		$this->assertFalse(BrValidation::ssn('50549727322'));
+		$this->assertFalse(BrValidation::ssn('869.283.422-11'));
+		$this->assertFalse(BrValidation::ssn('843.701.734-22'));
+
+		$this->assertTrue(BrValidation::ssn('22692173813'));
+		$this->assertTrue(BrValidation::ssn('50549727302'));
+		$this->assertTrue(BrValidation::ssn('869.283.422-00'));
+		$this->assertTrue(BrValidation::ssn('843.701.734-34'));
+	}
+
+}
+
+?>
