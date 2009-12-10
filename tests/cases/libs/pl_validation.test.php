68094845e956125552498e08f866aaf61bc8dfda
redd <marcin@awalo.pl>
2009-12-10 14:50:31 +0100

    added initial PlValidation rules and tests

---
+++ tests/cases/libs/pl_validation.test.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Polish Localized Validation class test case
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       localized
+ * @since         localized 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::import('Lib', 'Localized.PlValidation');
+
+class PlValidationTestCase extends CakeTestCase {
+
+/**
+ * test the postal method of PlValidation
+ *
+ * @return void
+ */
+	function testPostal() {
+		$this->assertTrue(PlValidation::postal('63-400'));
+		$this->assertFalse(PlValidation::postal('63400'));
+		$this->assertFalse(PlValidation::postal('634-00'));
+	}
+}
+?>
