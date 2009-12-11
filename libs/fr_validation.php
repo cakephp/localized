c316f033d5597b7534c34c56ffd3e68193364643
Matthieu Sadouni <matthieusadouni@gmail.com>
2009-12-11 09:39:30 +0100

    Social security numbers validation for France

--- libs/fr_validation.php
+++ libs/fr_validation.php
@@ -41,5 +41,43 @@ class FrValidation {
 		$pattern = '/^\d{5}$/';
 		return preg_match($pattern, $check);
 	}
+
+/**
+ * Checks social security numbers numbers for France
+ *
+ * @param string $check The value to check.
+ * @access public
+ * @return boolean
+ */
+	function ssn($check) {
+		$pattern = '/^[12]\d{2}(0\d|1[012])(\d{2}|2[AB])(\d{6}|\d{8})$/';
+		if (!preg_match($pattern, $check)) {
+			return false;
+		}
+
+		// No key to check
+		if (strlen($check) == 13) {
+			return true;
+		}
+
+		$numberWithoutKey = substr($check, 0, -2);
+		$key = substr($check, -2);
+
+		// Corse special cases
+		// source : http://xml.insee.fr/schema/nir.html
+		// check : http://www.parodie.com/monetique/nir.htm
+		if ($numberWithoutKey[6] == 'A') {
+			$numberWithoutKey = str_replace('A', '0', $numberWithoutKey);
+			$numberWithoutKey -= 1000000;
+		} elseif ($numberWithoutKey[6] == 'B') {
+			$numberWithoutKey = str_replace('B', '0', $numberWithoutKey);
+			$numberWithoutKey -= 2000000;
+		}
+
+		// bcmod works with large numbers where % doesn't
+		$expectedKey = 97 - bcmod($numberWithoutKey, 97);
+
+		return ($expectedKey == $key);
+	}
 }
 ?>
