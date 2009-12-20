83daed6c13c804c4a8fa7579acb9cd063f3c1bc8
predominant <graham@grahamweldon.com>
2009-12-20 22:09:44 +1100

    Adding Turkey Validation class. Added postal code validation for Turkey. Closes #6.

---
+++ libs/tr_validation.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Turkey Localised Validation class. Handles localised validation for Turkey
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       localized
+ * @subpackage    localized.libs
+ * @since         Localized Plugin v 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+/**
+ * TrValidation
+ *
+ * @package       localized
+ * @subpackage    localized.libs
+ */
+class TrValidation {
+
+/**
+ * Checks Postal Codes for Turkey
+ *
+ * @param string $check The value to check.
+ * @return boolean
+ * @access public
+ */
+	function postal($check) {
+		$pattern = '/^[0-9]{5}$/i';
+		return preg_match($pattern, $check);
+	}
+}
+?>
