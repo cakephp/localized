9357fa1982c98ed889821e2c6d0138b943aacc3d
Marko <flatline@home.ultimate.in.rs>
2010-01-27 18:18:36 +0100

    Added Serbian validation.

---
+++ libs/rs_validation.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Serbian Localized Validation class. Handles localized validation for Serbia
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       localized
+ * @subpackage    localized.libs
+ * @since         Localized Plugin v 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+/**
+ * RsValidation
+ *
+ * @package       localized
+ * @subpackage    localized.libs
+ */
+class RsValidation {
+
+/**
+ * Checks Postal Numbers (Poštanski broj) for Serbia
+ *
+ * @param string $check The value to check.
+ * @return boolean
+ * @access public
+ */
+	function postal_number($check) {
+		$pattern = '/^[0-9]{5}$/';
+		return preg_match($pattern, $check);
+	}
+
+/**
+ * Checks Address Codes (Adresni kod) for Serbia
+ *
+ * @param string $check The value to check.
+ * @return boolean
+ * @access public
+ */
+	function address_code($check) {
+		$pattern = '/^[0-9]{6}$/';
+		return preg_match($pattern, $check);
+	}
+
+/**
+ * Checks Unique Master Citizen Numbers (JMBG) for Serbia
+ *
+ * @param string $check The value to check.
+ * @return boolean
+ * @access public
+ * @link http://en.wikipedia.org/wiki/Unique_Master_Citizen_Number
+ */	
+	function jmbg($check) {
+		if (!preg_match('/^[0-9]{13}$/', $check)) {
+			return false;
+		}
+
+		list($a, $b, $c, $d, $e, $f, $g, $h, $i, $j, $k, $l, $m) = str_split($check);		
+		$checksum = 11 - ( 7 * ($a + $g) + 6 * ($b + $h) + 5 * ($c + $i) + 4 * ($d + $j) + 3 * ($e + $k) + 2 * ($f + $l) ) % 11;
+		return ($checksum == $m);
+	}
+}
+?>
---
+++ tests/cases/libs/rs_validation.test.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Serbian Localized Validation class test case
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       localized
+ * @subpackage    localized.tests.cases.libs
+ * @since         Localized Plugin v 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::import('Lib', 'Localized.RsValidation');
+
+/**
+ * RsValidationTestCase
+ *
+ * @package       localization
+ * @subpackage    localized.tests.cases.libs
+ */
+class RsValidationTestCase extends CakeTestCase {
+
+/**
+ * test the jmbg method of RsValidation
+ *
+ * @return void
+ * @access public
+ */
+	function testJMBG() {
+		$this->assertTrue(RsValidation::jmbg('1707017170007'));
+		$this->assertFalse(RsValidation::jmbg('1707017170008'));
+		$this->assertFalse(RsValidation::jmbg('170701717000'));
+		$this->assertFalse(RsValidation::jmbg('A707017170007'));
+	}
+
+/**
+ * test the postal_number method of RsValidation
+ *
+ * @return void
+ * @access public
+ */
+	function testPostalNumber() {
+		$this->assertTrue(RsValidation::postal_number('11090'));
+		$this->assertFalse(RsValidation::postal_number('111000'));
+		$this->assertFalse(RsValidation::postal_number('A1100'));
+	}
+
+/**
+ * test the address_code method of RsValidation
+ *
+ * @return void
+ * @access public
+ */
+	function testAddressCode() {
+		$this->assertTrue(RsValidation::address_code('122407'));
+		$this->assertFalse(RsValidation::address_code('11090'));
+		$this->assertFalse(RsValidation::address_code('A11090'));
+	}
+}
+?>
