2ac40ac470c7695fb7755830c774b5a165f2c3b1
lenzcom <dlenz@lenzcom.de>
2010-03-26 16:08:27 -0700

    Added function dob() which checks date of birth formal format for Germany (dd.mm.yyyy), afterwards checks it is a valid gregorian calendar date.

--- libs/de_validation.php
+++ libs/de_validation.php
@@ -24,29 +24,51 @@
  * @package       localized
  * @subpackage    localized.libs
  */
+
 class DeValidation {
 
-/**
- * Checks zipcodes for Germany
- *
- * @param string $check The value to check.
- * @return boolean
- * @access public
- */
-	function postal($check) {
-		$pattern = '/^[0-9]{5}$/';
-		return preg_match($pattern, $check);
-	}
-/**
- * Checks phone number for Germany
- *
- * @param string $check The value to check.
- * @return boolean
- * @access public
- */
-        function phone($check) {
-                $pattern = '/[0-9\/. \-]*/';
-		return preg_match($pattern, $check);
-        }
+  /**
+   * Checks zipcodes for Germany
+   *
+   * @param string $check The value to check.
+   * @return boolean
+   * @access public
+   */
+  function postal($check) {
+    $pattern = '/^[0-9]{5}$/';
+    return preg_match($pattern, $check);
+  }
+
+  /**
+   * Checks phone number for Germany
+   *
+   * @param string $check The value to check.
+   * @return boolean
+   * @access public
+   */
+  function phone($check) {
+    $pattern = '/[0-9\/. \-]*/';
+    return preg_match($pattern, $check);
+  }
+
+  /**
+   * Checks date of birth formal format for Germany (dd.mm.yyyy),
+   * afterwards checks it is a valid gregorian calendar date.
+   *
+   * @param string $check the date of birth.
+   * @return boolean
+   * @access public
+   */
+  function dob($check) {
+    $pattern = '/^\d{2}\.\d{2}\.(\d{2}|\d{4})$/';
+    $return = preg_match($pattern, $check);
+    if($return) {
+      $check = str_replace('.', ',', $check);
+    } else {
+      return false;
+    }
+    $check = explode(',', $check, 3);
+    return checkdate((int) $check[1], (int) $check[0], (int) $check[2]);
+  }
 }
 ?>
