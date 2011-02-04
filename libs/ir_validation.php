5e82930dd388f38a49d76ae648f361a3ba0522e5
Arash Hemmat <arash@arash-laptop.(none)>
2011-02-04 05:39:13 +0330

    Added alphaNumerci,numeric,cc and mobile method to the IrValidation class.

--- libs/ir_validation.php
+++ libs/ir_validation.php
@@ -5,12 +5,12 @@
  * PHP versions 4 and 5
  *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org
  * @package       localized
  * @subpackage    localized.libs
@@ -27,6 +27,43 @@
 class IrValidation {
 
 /**
+ * Check for Persian/Farsi characters and number an zero width non-joiner space.
+ * Also accpets latin numbers preventing potential problem until PHP becomes fully unicode compatible.
+ *
+ * @param string $check The value to check.
+ * @return boolean
+ * @access public
+ */
+	function alphaNumeric($check) {
+                $pattern = '/[^\x{0600}-\x{06FF}\x{FB50}-\x{FDFD}\x{FE70}-\x{FEFF}\x{0750}-\x{077F}0-9\s\x{200C}]+/u';
+		return !preg_match($pattern, $check);
+	}
+
+/**
+ * Checks for Persian/Farsi digits only and won't accept Arabic and Latin digits.
+ *
+ * @param string $check The value to check.
+ * @return boolean
+ * @access public
+ */
+	function numeric($check) {
+                $pattern = '/[^\x{06F0}-\x{06F9}\x]+/u';
+		return !preg_match($pattern, $check);
+	}
+
+/**
+ * Validation of Iran credit card numbers.
+ *
+ * @param string $check The value to check.
+ * @return boolean
+ * @access public
+ */
+	function cc($check) {
+                $pattern = '/[0-9]{4}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}$/';
+		return preg_match($pattern, $check);
+	}
+
+/**
  * Checks phone numbers for Iran
  *
  * @param string $check The value to check.
@@ -34,7 +71,19 @@ class IrValidation {
  * @access public
  */
 	function phone($check) {
-		$pattern = '/^[- .\(\)]?((98)|(\+98)|(0098)|0){1}[- .\(\)]{0,3}[1-9]{1}[0-9]{1,}[- .\(\)]*[0-9]{3,8}[- .\(\)]?$/';
+                $pattern = '/^[- .\(\)]?((98)|(\+98)|(0098)|0){1}[- .\(\)]{0,3}[1-9]{1}[0-9]{1,}[- .\(\)]*[0-9]{3,8}[- .\(\)]?$/';
+		return preg_match($pattern, $check);
+	}
+
+/**
+ * Checks mobile numbers for Iran
+ *
+ * @param string $check The value to check.
+ * @return boolean
+ * @access public 0 9 124061351
+ */
+	function mobile($check) {
+                $pattern = '/^[- .\(\)]?((98)|(\+98)|(0098)|0){1}[- .\(\)]{0,3}[{91}{93}]{1}[0-9]{8}$/';
 		return preg_match($pattern, $check);
 	}
 
@@ -59,24 +108,21 @@ class IrValidation {
  */
 	function ssn($check) {
 		$pattern = '/^\d{10}$/';
-		if (!preg_match($pattern, $check)) {
-			return false;
-		}
-		$sum = 0;
-		$equivalent = 0;
-		for ($i = 0; $i < 9; $i++) {
-			$sum += $check{$i} * (10 - $i);
-			if ($check{1} == $check{$i}) {
-				$equivalent++;
-			}
-		}
-		if ($equivalent == 10) {
-			return false;
-		}
-		$remaining = $sum % 11;
-		if ($remaining <= 1) {
-			return $remaining == $check{9};
-		}
-		return (11 - $remaining) == $check{9};
+                if (!preg_match($pattern, $check)) {
+                        return false;
+                }
+                $sum = 0;
+                $equivalent = 0;
+                for ($i = 0; $i < 9; $i++) {
+                      $sum+=$check{$i} * (10 - $i);
+                      if($check{1}==$check{$i}) $equivalent++;
+                }
+                if($equivalent==10) return false;
+                $remaining = $sum % 11;
+                if ($remaining <= 1) {
+                    return ($remaining==$check{9})?true:false;
+                }else{
+                    return ((11-$remaining)==$check{9})?true:false;
+                }
 	}
 }
--- tests/cases/libs/ir_validation.test.php
+++ tests/cases/libs/ir_validation.test.php
@@ -27,6 +27,56 @@ App::import('Lib', 'Localized.IrValidation');
  */
 class IrValidationTestCase extends CakeTestCase {
 
+
+/**
+ * test the alphaNumeric method of IrValidation
+ *
+ * @return void
+ * @access public
+ */
+	function testAlphaNumeric() {
+		$this->assertTrue(IrValidation::phone('آزمایش ۱۲۳۴۵۶۷۸۹۰'));
+		$this->assertTrue(IrValidation::phone('آزمایش 1234567890'));
+		$this->assertTrue(IrValidation::phone('هِمّت بُلَند دار کِه مَردانِ روزگار  اَز همّتِ بُلَند به جایی رسیده‌اَند'));
+		$this->assertTrue(IrValidation::phone('﷼'));
+		
+		$this->assertFalse(IrValidation::phone('teststring'));
+		$this->assertFalse(IrValidation::phone('test1234567890'));
+		$this->assertFalse(IrValidation::phone('test آزمایش'));
+	}
+
+/**
+ * test the numeric method of IrValidation
+ *
+ * @return void
+ * @access public
+ */
+	function testNumeric() {
+		$this->assertTrue(IrValidation::phone('۱۲۳۴۵۶۷۸۹۰'));
+
+		$this->assertFalse(IrValidation::phone('teststring'));
+		$this->assertFalse(IrValidation::phone('1234567890'));
+		$this->assertFalse(IrValidation::phone('١٢٣٤٥٦٧٨٩٠'));
+		
+	}
+
+/**
+ * test the cc method of IrValidation
+ *
+ * @return void
+ * @access public
+ */
+	function testCc() {
+		$this->assertTrue(IrValidation::phone('1111222233334444'));
+		$this->assertTrue(IrValidation::phone('1111-2222-3333-4444'));
+		$this->assertTrue(IrValidation::phone('1111 2222 3333 4444'));
+
+		$this->assertFalse(IrValidation::phone('teststring'));
+		$this->assertFalse(IrValidation::phone('1111'));
+		$this->assertFalse(IrValidation::phone('111-122-223-333-444-4'));
+		
+	}
+
 /**
  * test the phone method of IrValidation
  *
@@ -58,6 +108,29 @@ class IrValidationTestCase extends CakeTestCase {
 		$this->assertFalse(IrValidation::phone('+98/21/33334444'));
 		$this->assertFalse(IrValidation::phone('+02133334444'));
 		$this->assertFalse(IrValidation::phone('021 22'));
+	}
+
+/**
+ * test the mobile method of IrValidation
+ *
+ * @return void
+ * @access public
+ */
+	function testMobile() {
+		$this->assertTrue(IrValidation::phone('989123334444'));
+		$this->assertTrue(IrValidation::phone('00989353334444'));
+		$this->assertTrue(IrValidation::phone('+989363334444'));
+		$this->assertTrue(IrValidation::phone('+98 9373334444'));
+		$this->assertTrue(IrValidation::phone('(+98) 9383334444'));
+		$this->assertTrue(IrValidation::phone('+98-9323334444'));
+
+		$this->assertFalse(IrValidation::phone('teststring'));
+		$this->assertFalse(IrValidation::phone('999123334444'));
+		$this->assertFalse(IrValidation::phone('+999353334444'));
+		$this->assertFalse(IrValidation::phone('00999363334444'));
+		$this->assertFalse(IrValidation::phone('000989373334444'));
+		$this->assertFalse(IrValidation::phone('+00989383334444'));
+		$this->assertFalse(IrValidation::phone('+0989323334444'));
 	}
 
 /**
