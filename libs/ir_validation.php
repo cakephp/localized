b3d5c80062bfe7deeca15a9ba70ef348962d24e3
Arash Hemmat <arash@arash-laptop.(none)>
2011-02-04 05:52:02 +0330

    Copyright updated.

--- libs/ir_validation.php
+++ libs/ir_validation.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * Iranian Localized Validation class. Handles localized validation for Iran
- *
- * PHP versions 4 and 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org
- * @package       localized
- * @subpackage    localized.libs
- * @since         Localized Plugin v 0.1
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
+* Iranian Localized Validation class. Handles localized validation for Iran
+*
+* PHP versions 4 and 5
+*
+* CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+* Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+*
+* Licensed under The MIT License
+* Redistributions of files must retain the above copyright notice.
+*
+* @copyright Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+* @link http://cakephp.org
+* @package localized
+* @subpackage localized.libs
+* @since Localized Plugin v 0.1
+* @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+*/
 
 /**
  * IrValidation
