60f00f1be30a52c85da2a5d1fe96112879e2b855
Michael Enger <mien@nodesagency.no>
2012-10-23 11:10:31 +0200

    Add Norwegian validation and test

---
+++ Lib/NoValidation.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Norwegian Localized Validation class. Handles localized validation for Norway
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       Localized.Lib
+ * @since         Localized Plugin v 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+/**
+ * NoValidation
+ *
+ * @package       Localized.Lib
+ */
+class NoValidation {
+
+/**
+ * Checks social security numbers for Norway
+ *
+ * @param string $check The value to check.
+ * @return boolean
+ */
+	public static function ssn($check) {
+		$pattern = '/[0-9]{11}/';
+		return (bool)preg_match($pattern, $check);
+	}
+}
---
+++ Test/Case/Lib/NoValidationTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Norwegian Localized Validation class test case
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       Localized.Test.Case.Lib
+ * @since         Localized Plugin v 0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::uses('NoValidation', 'Localized.Lib');
+
+/**
+ * NoValidationTest
+ *
+ * @package       Localized.Test.Case.Lib
+ */
+class NoValidationTest extends CakeTestCase {
+
+/**
+ * test the ssn method of NoValidation
+ *
+ * @return void
+ */
+	public function testSsn() {
+		$this->assertTrue(NoValidation::ssn('12345678901'));
+		$this->assertFalse(NoValidation::ss('123456 12345'));
+	}
+}
