4e33f085a756006664b45050dcc95383fdde01cf
cake17 <root@email.com>
2013-04-10 09:35:58 +0200

    [fr] add FrValidation dob method

--- Validation/FrValidation.php
+++ Validation/FrValidation.php
@@ -73,4 +73,23 @@ class FrValidation {
 		}
 		return $key == (97 - ($numberWithoutKey - (floor($numberWithoutKey / 97) * 97)));
 	}
+
+/**
+ * Checks date of birth formal format for French (dd/mm/yyyy),
+ * afterwards checks it is a valid gregorian calendar date.
+ *
+ * @param string $check the date of birth.
+ * @return boolean
+ */
+	public static function dob($check) {
+		$pattern = '/^\d{1,2}\/\d{1,2}\/(\d{2}|\d{4})$/';
+		$return = preg_match($pattern, $check);
+		if ($return) {
+			$check = str_replace('/', ',', $check);
+		} else {
+			return false;
+		}
+		$check = explode(',', $check, 3);
+		return checkdate((int)$check[1], (int)$check[0], (int)$check[2]);
+	}
 }
