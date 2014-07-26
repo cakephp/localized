866c82f5c160c72fba2f02cfc91a40f04a474938
Aaron Tate <fenix@f3n.org>
2014-07-27 01:02:47 +1000

    Fix Missing Parentheses, Indentation via Tab
    Added missing paretheses at end of regex pattern.
    Replaced space indentation with Tab indentation.

--- Validation/AuValidation.php
+++ Validation/AuValidation.php
@@ -42,8 +42,8 @@ class AuValidation {
 * @return boolean
 */
         public static function phone($check) {
-        	$normalized = preg_replace('/(\s+|-|\(|\))/', '', preg_replace('/0011\s?61/','+61',$check));  //remove spaces, parentheses and hyphens, convert full intl prefix.
-        	$pattern = '/^(((0|\+61)[2378])(\d){8}|((0|\+61)[45](\d){2}|1300|1800|190[02])(\d){6}|(\+61)180(\d){4}|(\+61)?13\d{4}|(\+61)?12[2-8](\d){1,7}|(\+61|0)14[12357](\d){6}$/';
-                return (bool)preg_match($pattern, $normalized);
+		$normalized = preg_replace('/(\s+|-|\(|\))/', '', preg_replace('/0011\s?61/','+61',$check));  //remove spaces, parentheses and hyphens, convert full intl prefix.
+		$pattern = '/^(((0|\+61)[2378])(\d){8}|((0|\+61)[45](\d){2}|1300|1800|190[02])(\d){6}|(\+61)180(\d){4}|(\+61)?13\d{4}|(\+61)?12[2-8](\d){1,7}|(\+61|0)14[12357](\d){6})$/';
+		return (bool)preg_match($pattern, $normalized);
         }
 }
