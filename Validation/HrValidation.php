35e65967646822c8f8c3b7d7f833ac7036ff7f08
Mark Scherer <euromark@web.de>
2015-05-19 13:08:51 +0200

    Add postal validation for Croatia.

---
+++ Validation/HrValidation.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Localized Validation class. Handles localized validation for Croatia.
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @since         Localized Plugin v 0.1
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+App::uses('LocalizedValidation', 'Localized.Validation');
+
+/**
+ * HrValidation
+ *
+ */
+class HrValidation extends LocalizedValidation {
+
+/**
+ * Checks a postal code for Croatia.
+ *
+ * @param string $check The value to check.
+ * @return bool Success.
+ */
+	public static function postal($check) {
+		$pattern = '/^\d{5}$/';
+		return (bool)preg_match($pattern, $check);
+	}
+
+/**
+ * Checks a phone number.
+ *
+ * @param string $check The value to check.
+ * @return bool Success.
+ * @throws NotImplementedException
+ */
+	public static function phone($check) {
+		throw new NotImplementedException('Validation method not implemented yet.');
+	}
+
+/**
+ * Checks a country specific identification number.
+ *
+ * @param string $check The value to check.
+ * @return bool Success.
+ * @throws NotImplementedException
+ */
+	public static function personId($check) {
+		throw new NotImplementedException('Validation method not implemented yet.');
+	}
+
+}
