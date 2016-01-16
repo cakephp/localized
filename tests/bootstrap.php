6917b26a05cc187dee26e600f095d09ca4a13636
Mark Scherer <euromark@web.de>
2016-01-16 22:42:11 +0100

    tests/bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,12 +8,12 @@ define('APP_DIR', 'src');
 
 define('APP', rtrim(sys_get_temp_dir(), DS) . DS . APP_DIR . DS);
 if (!is_dir(APP)) {
-	mkdir(APP, 0770, true);
+    mkdir(APP, 0770, true);
 }
 
 define('TMP', ROOT . DS . 'tmp' . DS);
 if (!is_dir(TMP)) {
-	mkdir(TMP, 0770, true);
+    mkdir(TMP, 0770, true);
 }
 
 define('CONFIG', dirname(__FILE__) . DS . 'config' . DS);
@@ -25,51 +25,50 @@ define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
 define('CAKE', CORE_PATH . 'src' . DS);
 
 Cake\Core\Configure::write('App', [
-	'namespace' => 'App'
+    'namespace' => 'App'
 ]);
 
 Cake\Core\Configure::write('debug', true);
 
 $cache = [
-	'default' => [
-		'engine' => 'File',
-		'path' => CACHE
-	],
-	'_cake_core_' => [
-		'className' => 'File',
-		'prefix' => 'crud_myapp_cake_core_',
-		'path' => CACHE . 'persistent/',
-		'serialize' => true,
-		'duration' => '+10 seconds'
-	],
-	'_cake_model_' => [
-		'className' => 'File',
-		'prefix' => 'crud_my_app_cake_model_',
-		'path' => CACHE . 'models/',
-		'serialize' => 'File',
-		'duration' => '+10 seconds'
-	]
+    'default' => [
+        'engine' => 'File',
+        'path' => CACHE
+    ],
+    '_cake_core_' => [
+        'className' => 'File',
+        'prefix' => 'crud_myapp_cake_core_',
+        'path' => CACHE . 'persistent/',
+        'serialize' => true,
+        'duration' => '+10 seconds'
+    ],
+    '_cake_model_' => [
+        'className' => 'File',
+        'prefix' => 'crud_my_app_cake_model_',
+        'path' => CACHE . 'models/',
+        'serialize' => 'File',
+        'duration' => '+10 seconds'
+    ]
 ];
 
 Cake\Cache\Cache::config($cache);
 
-//needed?
 Cake\Core\Plugin::load('Localized', ['path' => ROOT . DS]);
 
 // Ensure default test connection is defined
 if (!getenv('db_class')) {
-	putenv('db_class=Cake\Database\Driver\Sqlite');
-	putenv('db_dsn=sqlite::memory:');
+    putenv('db_class=Cake\Database\Driver\Sqlite');
+    putenv('db_dsn=sqlite::memory:');
 }
 
 Cake\Datasource\ConnectionManager::config('test', [
-	'className' => 'Cake\Database\Connection',
-	'driver' => getenv('db_class'),
-	'dsn' => getenv('db_dsn'),
-	'database' => getenv('db_database'),
-	'username' => getenv('db_username'),
-	'password' => getenv('db_password'),
-	'timezone' => 'UTC',
-	'quoteIdentifiers' => true,
-	'cacheMetadata' => true,
+    'className' => 'Cake\Database\Connection',
+    'driver' => getenv('db_class'),
+    'dsn' => getenv('db_dsn'),
+    'database' => getenv('db_database'),
+    'username' => getenv('db_username'),
+    'password' => getenv('db_password'),
+    'timezone' => 'UTC',
+    'quoteIdentifiers' => true,
+    'cacheMetadata' => true,
 ]);
