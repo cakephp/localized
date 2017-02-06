c4e74071a49db81d54eca21d20f6289d62f9c918
Burak USGURLU <burak@uskur.com.tr>
2017-02-06 23:09:39 +0200

    strict checks

--- src/Validation/TrValidation.php
+++ src/Validation/TrValidation.php
@@ -45,11 +45,11 @@ class TrValidation extends LocalizedValidation
     public static function personId($check)
     {
         $tckn = array_shift(array_values($check));
-        if (strlen($tckn) != 11) {
+        if (strlen($tckn) !== 11) {
             return false;
         }
 
-        if ($tckn[0] == 0) {
+        if ($tckn[0] === 0) {
             return false;
         }
 
@@ -57,13 +57,13 @@ class TrValidation extends LocalizedValidation
         $odd = $tckn[1] + $tckn[3] + $tckn[5] + $tckn[7];
         $check = ($even * 7) - $odd;
 
-        if ($check % 10 != $tckn[9]) {
+        if ($check % 10 !== $tckn[9]) {
             return false;
         }
 
         $check = $even + $odd + $tckn[9];
 
-        if ($check % 10 != $tckn[10]) {
+        if ($check % 10 !== $tckn[10]) {
             return false;
         }
 
